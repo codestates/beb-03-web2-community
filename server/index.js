430ea2206a6f9659ac5e416040eed73e1af753a4
Rezalog <rladlfdnd369@naver.com>
2022-04-19 18:04:14 +0900

    build(김일웅): setting server, mongoDB & atlas

--- server/index.js
+++ server/index.js
@@ -2,11 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 8080;
-
- const indexRouter = require("./routes/index");
+const mongoose = require('mongoose');
+const indexRouter = require("./routes/index");
 // const etherRouter = require("./routes/ether");
 // const accountRouter = require("./routes/account");
+// mongosh "mongodb+srv://cluster0.ej0vx.mongodb.net/community" --apiVersion 1 --username web2-community
 
+mongoose.connect(`mongodb+srv://web2-community:1234@cluster0.ej0vx.mongodb.net/community`)
+.then(() => console.log('MongoDB connected...'))
+.catch(error => console.log(error))
+
+// const db = mongoose.connection;
+// db.on('err', console.error);
+// db.once('open', function() {
+// console.log('connected mongodb server')
+// });
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
