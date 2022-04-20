68ac33a82ca18f009446c365c3e92fa42b650bf6
EllaDev <ellawork23@gmail.com>
2022-04-20 09:55:44 +0900

    feat(윤은주): setting tailwindCSS

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    backgroundColor: (theme) => ({
+      ...theme('colors'),
+      
+    }),
+  },
+  variants: {},
+  plugins: [],
+};
