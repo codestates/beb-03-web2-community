3d7bf76d805491da78b530451a564425e7fdc943
EllaDev <ellawork23@gmail.com>
2022-04-21 13:16:46 +0900

    chore(윤은주): Forgot upload for context API

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { StoreProvider } from './context';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StoreProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StoreProvider>,
   document.getElementById('root')
 );
 
