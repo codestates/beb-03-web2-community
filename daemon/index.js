3b8d929ca3a98a4111912d7b914434f37703baff
djLeeee <ehdwo115@gmail.com>
2022-04-26 17:24:32 +0900

    build(이동재): daemon server finish

--- daemon/index.js
+++ daemon/index.js
@@ -1,4 +1,16 @@
 const myWeb3= require('./myWeb3');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const {USER_NAME, USER_PASSWORD, CLOUD_NAME, DATABASE_NAME } = process.env;
+const MONGO_URI = `mongodb+srv://${USER_NAME}:${USER_PASSWORD}@${CLOUD_NAME}.ej0vx.mongodb.net/${DATABASE_NAME}`
 
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log('MongoDB connected...')
+    
+})
+.catch(error => console.log(error));
 
-console.log(myWeb3.getLastestTransactions());
+myWeb3.getLastestTransactions();   
+
+//
--- daemon/myWeb3.js
+++ daemon/myWeb3.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+const users = require('./models/users');
+
 const fs = require('fs');
 
 const {PROVIDER, ABI_FILE_PATH, ADDRESS_FILE_PATH} = process.env;
@@ -15,7 +18,6 @@ const checkedBlockNum = Number(
 
 //contract주소 가져오기
 var addressFile = ADDRESS_FILE_PATH;
-console.log(ADDRESS_FILE_PATH);
 var contractAddress = fs.readFileSync(addressFile).toString();
 
 //ABI Decoder 추가
@@ -23,6 +25,9 @@ var abiFile = ABI_FILE_PATH;
 var contractAbi= JSON.parse(fs.readFileSync(abiFile));
 const abiDecoder = require('abi-decoder'); // NodeJS
 abiDecoder.addABI(contractAbi);
+
+//컨트랙트 연결
+const contract= require('../server/contract/contract');
 
 //tx리스트
 const allTransactions = [];
@@ -33,6 +38,9 @@ const getTx = async (tx) => await web3.eth.getTransaction(tx);
 module.exports.getLastestTransactions  = async function getLastestTransactions(){
     try {
 		let lastest = checkedBlockNum;
+		
+		console.log("NowBlockNumaber:"+lastest);
+
 		// 최신 블록번호가 마지막에 확인한 블록번호보다 크다면,
 		// 그 차이만큼 블록을 조회하기 위해 범위 업데이트
 		await web3.eth.getBlockNumber((err, result) => {
@@ -46,40 +54,44 @@ module.exports.getLastestTransactions  = async function getLastestTransactions()
 			return [];
 		} else {
 			// 가장 마지막에 확인한 블록의 다음 블록부터 가장 최신 블록까지의 모든 트랜잭션 조회
-			//for (let i = checkedBlockNum + 1; i <= lastest; i++) {
+			for (let i = checkedBlockNum + 1; i <= lastest; i++) {
 				
 				//현재 한 블록만 테스트중
-				const block = await web3.eth.getBlock(12217381);
-
+				const block = await web3.eth.getBlock(i);
 				// 트랜잭션 해시로 모든 트랜잭션 조회
 				for (let tx of block.transactions) {
-					allTransactions.push(getTx(tx));
+					allTransactions.push(await getTx(tx));
 				}
-			//}
-			console.log(contractAddress);
-			// 모든 트랜잭션 중에서, 조건에 부합하는 트랜잭션을 배열로 리턴(Promise)
-			return Promise.all(allTransactions)
-				.then((data) => {
-					const result = [];
-					for (let tx of data) {
-						if (tx.from.toLowerCase() === contractAddress || tx.to.toLowerCase() === contractAddress) {
-							console.log(abiDecoder.decodeMethod(tx.input))
-							//해당 영수증을 조회했을때 to 유저에게 1000을 더 할것??
-							result.push(abiDecoder.decodeMethod(tx.input));
-						}
-					}
-					return result;
-				})
-				.then((data) => {
-					// 가장 마지막에 확인한 블록번호 저장
-					fs.writeFileSync(
-						'./blockNumber',
-						String(lastest),
-					);
-					return data;
-				});
+			}
+			for(let tx of allTransactions){
+				if(tx.from==null || tx.to==null){
+					console.log("No balance Data")
+					continue;
+				}
+				else if(tx.from.toLowerCase() === contractAddress.toLowerCase() || tx.to.toLowerCase() === contractAddress.toLowerCase()) {
+					
+					let decodeData =abiDecoder.decodeMethod(tx.input);
+					let to = decodeData.params[0].value;
+					let toBalance = await contract.getBalance(to);
+					
+					//해당 주소의 잔액 업데이트 
+					await users.updateOne({address:to},{balance:toBalance});
+				}else{
+					console.log("No balance Data")
+				}
+			}
+
+			//조회한 블록번호 파일에 저장
+			fs.writeFileSync(
+				'./blockNumber',
+				String(lastest),
+			);
 		}
+		
 	} catch (err) {
 		console.log(err);
+	} finally{
+		//mongoose db 종료
+		mongoose.connection.close();
 	}
 }
