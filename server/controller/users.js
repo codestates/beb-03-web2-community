db0aa8f833bcc578f7f3964a3665221f6a7a159e
Rezalog <rladlfdnd369@naver.com>
2022-04-26 12:48:41 +0900

    feat(김일웅): added userInfo-get module, modified singin-post module with jwt generating and saving at response cookie

--- server/controller/users.js
+++ server/controller/users.js
@@ -12,14 +12,7 @@ exports.signup = async (req, res) => {
     if(!(userEmail && userName && password)) {
         return res.status(400).send('Bad Request : 회원가입 정보를 올바르게 입력해야합니다.')
     }
-    // 2. findOne 으로 DB에 이미 있는 회원인지(userName 중복) 조회 => 있으면 'user already exists' send
-    //-- 
-    // await users.findOne({ userEmail: userEmail }, (err, msg) => {
-    //     if(err) {
-
-    //     }
-    // })
-
+    
     try {
 
         // 3. DB save
@@ -47,37 +40,80 @@ exports.signup = async (req, res) => {
 
 exports.signin = async (req, res) => {
     
+
+    /* 이전 코드
+    // await users.find({ $and:[{ userEmail: userEmail }, { password: password }] })
+    // .then((result)=>{
+    //     if(result.length===0) return res.json({ message: '잘못된 이메일이나 비밀번호 입니다.' })
+    //     else {
+    //         return res.json({result: result[0]})
+    //     }
+    // })
+
+    */
+
+    // 입력받은 회원정보(이메일, 패스워드)가 db에 match 되는지 체크
+    // (각 정보마다 match 되지 않을 경우 다른 json send, 비밀번호는 암호화하여 전송 후 비교)
+    // match 되는 경우, jwt token 생성
+
     // 1. req.body에 userEmail, password를 입력받지 않았을 경우
     const { userEmail, password } = req.body;
     if(!(userEmail && password)) return res.status(400).send({message: 'Bad Request: 회원정보를 입력하세요.'})
-
-    await users.find({ $and:[{ userEmail: userEmail }, { password: password }] })
-    .then((result)=>{
-        if(result.length===0) return res.json({ message: '잘못된 이메일이나 비밀번호 입니다.' })
-        else {
-            return res.json({result: result[0]})
+ 
+    // 2. userEmail DB 비교
+    await users.findOne({ userEmail: userEmail }, (err, userData) => {
+        if (err) {
+          return res.json({
+            loginSuccess: false,
+            message: "존재하지 않는 아이디입니다.",
+          });
         }
-    })
+        // 3. comparePassword  in DB
+        await userData
+          .comparePassword(password)
+          .then((isMatch) => {
+            if (!isMatch) {
+              return res.json({
+                loginSuccess: false,
+                message: "비밀번호가 일치하지 않습니다",
+              });
+            }
+        // 4. 회원정보 모두 일치시 token generate
+        await userData
+              .generateToken()
+              .then((userData) => {
+
+                // cookie 에 token(_id) 저장
+                res
+                  .cookie("x_auth", userData.token)
+                  .status(200)
+                  .json({ loginSuccess: true, user_Id: userData._id });
+
+              }) // end generateToken
+              .catch((err) => {
+                res.status(400).send(err);
+              });
+
+          }) // end comparePassword.then
+          .catch((err) => res.json({ loginSuccess: false, err }));
     
-    // 추후 구현
-    // 2. userEmail, password를 입력받아 db에서 match 여부를 돌려준다
-        // 2-1. userEmail match 확인
-    // users.findOne({userEmail: userEmail}, (err, user) => {
-    //     if(!user) return res.json({loginSuccess: false, message: "등록되지 않은 이메일입니다."})
-    // })
-        // 2-2. password match 확인
-    // users.comparePassword(password, (err, isMatch) => {
-    //     if (!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다" });
-    // })
-    // 3. token 생성
-    // users.generateToken
+    }); // end find Email
+}
 
-    // 4. 쿠키에 token 저장
+exports.userInfo = async (req, res, next) => {
 
-};
-
-exports.userInfo = async (req, res) => {
-
-
-    // await users.findOne
+    // cookie-parser로 cookie의 token 을 받아 회원정보를 조회한다.
+    let token = req.cookies.x_auth;
+    
+    await users
+        .findByToken(token)
+        .then((user) => {
+        if (!user) return res.json({ isAuth: false, error: true });
+          req.token = token;
+          req.user = user;
+          next();
+        })
+        .catch((err) => {
+          res.status(400).send(err);
+        });
 };
